f2386d3eb9465ef4b027eb68646b73e67b898e53
Alexandre Henrique <alex.henri.2008@gmail.com>
2026-09-15 18:38:17 -0300

    teste massa

--- Aprendizado de Linguagens/Estudos C/primeiroarquivo.c
+++ Aprendizado de Linguagens/Estudos C/primeiroarquivo.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int idade = 20;
+float nota;
+
+
 int main() {
     printf("Deu certo! Meu ambiente C esta rodando perfeitamente!\n");
-    imputf('Qual seu pé favorito?');
+
+    printf("Sua idade é %d\n", idade);
+    printf("Digite sua nota: ");
+    scanf("%f", &nota);
     return 0;
 }
