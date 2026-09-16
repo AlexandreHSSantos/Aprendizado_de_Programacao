9760e819589edf0a94adab0f973b193cfd1ad2a1
Alexandre Henrique <alex.henri.2008@gmail.com>
2026-09-16 19:34:35 -0300

    aplicação de FOR

---
+++ Aprendizado de Linguagens/Estudos C/utilizacao_de_lacos_repeticao.c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    printf("Contando de 1 a 5 com FOR:\n");
+    for (i = 1; i<= 5; i++) {
+        printf("Número: %d\n", i);
+    }
+
+
+
+
+
+
+
+ 
+
+
+    return 0;
+}
