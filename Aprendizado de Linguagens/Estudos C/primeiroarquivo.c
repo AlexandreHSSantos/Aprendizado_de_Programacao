16226a5be6ca8beb5196bc3f1715064f497ed452
Alexandre Henrique <alex.henri.2008@gmail.com>
2026-09-16 17:52:14 -0300

    trocando int por float

--- Aprendizado de Linguagens/Estudos C/primeiroarquivo.c
+++ Aprendizado de Linguagens/Estudos C/primeiroarquivo.c
@@ -2,7 +2,7 @@
 
 int idade = 20;
 float nota;
-
+float *p = &nota;
 
 int main() {
     printf("Deu certo! Meu ambiente C esta rodando perfeitamente!\n");
@@ -10,5 +10,9 @@ int main() {
     printf("Sua idade é %d\n", idade);
     printf("Digite sua nota: ");
     scanf("%f", &nota);
+    
+    *p = 2;
+    
+    printf("Sua nota é %f\n", nota);
     return 0;
 }
