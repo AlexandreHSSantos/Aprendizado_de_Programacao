962ba9446e0d0e8dba4ea123524d4d4e6857cdeb
Alexandre Henrique <alex.henri.2008@gmail.com>
2026-09-16 18:12:56 -0300

    usuário responde as questões

--- Aprendizado de Linguagens/Estudos C/primeiroarquivo.c
+++ Aprendizado de Linguagens/Estudos C/primeiroarquivo.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-
-int idade = 20;
+int idade;
 float nota;
 float *p = &nota;
 
 int main() {
     printf("Deu certo! Meu ambiente C esta rodando perfeitamente!\n");
 
-    printf("Sua idade é %d\n", idade);
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+
     printf("Digite sua nota: ");
     scanf("%f", &nota);
     
-    *p = 2;
-    
+    printf("\n--- Dados Cadastrados ---\n");
+    printf("Idade: %d anos\n", idade);
     printf("Sua nota é %f\n", nota);
     return 0;
 }
